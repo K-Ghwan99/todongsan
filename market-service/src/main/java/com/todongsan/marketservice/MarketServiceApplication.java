371216e44a61178ea0d8e079aeaa920e0f891d6d
asd1702 <gyeonghyeongib@gmail.com>
2026-05-31 17:52:38 +0900

    feat: init market-service spring boot project

---
+++ market-service/src/main/java/com/todongsan/marketservice/MarketServiceApplication.java
@@ -0,0 +1,15 @@
+package com.todongsan.marketservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MarketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketServiceApplication.class, args);
+    }
+
+}
